09081833d97854c0c5506a54b6ca544bdd4842f1
tkhubert <tkhubert@gmail.com>
2015-02-01 17:14:05 +0000

    Initial Commit

---
+++ StanfordAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StanfordAlgorithm
+//
+//  Created by Thomas Hubert on 01/02/2015.
+//  Copyright (c) 2015 Thomas Hubert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
